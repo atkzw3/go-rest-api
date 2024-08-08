aed64ab87a4f4dbeb60dd3bc77f043cc6ecd6129
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-08 23:14:28 +0900

    :tada: first commit

---
+++ model/user.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// UserResponse クライアントにレスポンスする際のデータ型
+type UserResponse struct {
+	ID    uint   `json:"id" gorm:"primaryKey"`
+	Email string `json:"email" gorm:"unique"`
+}
